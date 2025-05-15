50d181ae33e406114432587c1b40818b815e44f6
augusgerco <augustosgercovich@gmail.com>
2025-05-15 12:09:12 -0300

    COMPLETO

--- Assets/Scripts/ClonaCubos.cs
+++ Assets/Scripts/ClonaCubos.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class ClonaCubos : MonoBehaviour
 {
+    public GameObject cuboPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void ClonarCubo()
+    {
+        Instantiate(cuboPrefab);
     }
 }
